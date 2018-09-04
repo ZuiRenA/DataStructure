bee6326d810c2b2febadb4839bec1b67713333a0
ZuiRen <a13405665741@gmail.com>
2018-09-05 01:07:02 +0800

    二分法、逆序排列和删除重复数

--- ArrayPractice/HighArray.java
+++ ArrayPractice/HighArray.java
@@ -1,5 +1,5 @@
 package JavaPractice.ArrayPractice;
-/*
+
 class HighArray {
     private long[] a;
     private int nElems;
@@ -53,38 +53,113 @@ class HighArray {
         }
         System.out.println();
     }
+
+    public long getMax(){
+        long max = -1;
+        for (int i = 0; i < nElems; i++){
+            if (a[i] > max){
+                max = a[i];
+            }
+        }
+        return max;
+    }
+
+    public long removeMax(){
+        long max = -1;
+        int index = 0;
+        for (int i = 0; i < nElems; i++){
+            if (a[i] > max){
+                max = a[i];
+                index = i;
+            }
+        }
+        if (index != -1){
+            for (int i = index + 1; i < nElems; i++){
+                a[i] = a[i+1];
+            }
+            nElems --;
+        }
+        return max;
+    }
+
+    public void noDup(){
+        int NULL = -1;
+        int order = 0;
+
+        for (int j = 0; j < nElems; j++){
+            for (int i = j + 1; i < nElems; i++){
+                if (a[j] != NULL && a[j] == a[i]){
+                    a[i] = NULL;
+                }
+            }
+        }
+
+        for (int temp = 0; temp < nElems; temp++){
+            if (a[temp] != NULL){
+                if (temp > order){
+                    a[order] = a[temp];
+                }
+                order ++;
+            }
+        }
+
+        nElems = order;
+    }
+
 }
 
 class HighArrayApp{
     public static void main(String[] args){
         int maxSize = 100;
-        HighArray array = new HighArray(maxSize);
+        HighArray array1 = new HighArray(maxSize);
 
-        array.insert(00);
-        array.insert(11);
-        array.insert(22);
-        array.insert(33);
-        array.insert(44);
-        array.insert(55);
-        array.insert(66);
-        array.insert(77);
-        array.insert(88);
-        array.insert(99);
+        array1.insert(00);
+        array1.insert(11);
+        array1.insert(22);
+        array1.insert(33);
+        array1.insert(44);
+        array1.insert(55);
+        array1.insert(66);
+        array1.insert(77);
+        array1.insert(88);
+        array1.insert(99);
+        array1.insert(99);
+        array1.insert(99);
 
-        array.display();
+        array1.display();
 
+        System.out.println("删除重复数");
+        array1.noDup();
+        array1.display();
+
+        /*
         int searchKey = 35;
-        if (array.find(searchKey)){
+        if (array1.find(searchKey)){
             System.out.println("Found " + searchKey);
         }else {
             System.out.println("Con't find " + searchKey);
         }
 
-        array.delete(00);
-        array.delete(55);
-        array.delete(99);
+        array1.delete(00);
+        array1.delete(55);
+        array1.delete(99);
 
-        array.display();
+        array1.display();
+        */
+
+
+        long max = array1.getMax();
+        System.out.println("Found max is " + max);
+
+        HighArray array2 = new HighArray(maxSize);
+        int i = 0;
+        max = array1.removeMax();
+        while (max != -1){
+            array2.insert(max);
+            max = array1.removeMax();
+        }
+        System.out.println("逆序排列");
+        array2.display();
+
     }
 }
-*/
--- ArrayPractice/OrderArray.java
+++ ArrayPractice/OrderArray.java
@@ -1,5 +1,9 @@
 package JavaPractice.ArrayPractice;
 
+import org.mvel2.ast.Or;
+
+import java.util.Random;
+
 class OrdArray{
     private long[] a;
     private int nElems;
@@ -36,16 +40,59 @@ class OrdArray{
 
     public void insert(long value){
         int i;
-        for (i = 0; i < nElems; i++){
-            if (a[i] > value){
+        for(i = 0;i < nElems;i++){
+            //find where it goes
+            if(a[i] > value){//line search
                 break;
             }
         }
-        for (int k = nElems; k > i; k--){
+        for(int k = nElems;k > i;k--){
+            //move bigger ones up
             a[k] = a[k-1];
         }
         a[i] = value;
-        nElems ++;
+        nElems++;
+    }
+
+    public void insert1(long value){
+       int lowerBound = 0;
+       int upperBound = nElems - 1;
+       int curIn;
+
+       while (true){
+           curIn = (lowerBound + upperBound) / 2;
+           if (lowerBound > upperBound){
+               break;
+           }
+           if (a[curIn] == value){
+               break;
+           }else if (a[curIn] < value){
+               if (curIn == nElems - 1){
+                    curIn = curIn + 1;
+                    break;
+               }else if (a[curIn+1] >= value){
+                   curIn = curIn + 1;
+                   break;
+               }else {
+                   lowerBound = curIn - 1;
+               }
+           }else {
+               if (curIn == 0){
+                   break;
+               }else if (a[curIn - 1] < value){
+                   break;
+               }else {
+                   upperBound = curIn + 1;
+               }
+
+           }
+       }
+
+       for (int i = nElems; i > curIn; i--){
+           a[i] = a[i-1];
+       }
+       a[curIn] = value;
+       nElems ++;
     }
 
     public boolean delete(long value){
@@ -53,12 +100,37 @@ class OrdArray{
         if (i == nElems){
             return false;
         }else {
-            for (int k = i; k < nElems; k++){
-                a[k] = a[k+1];
+            for (int j = i;j < nElems; j++){
+                a[j] = a[j+1];
             }
             nElems --;
             return true;
         }
+        /*
+        int lowerBound = 0;
+        int upperBound = nElems - 1;
+        int curIn;
+
+        while (true){
+            curIn = (lowerBound + upperBound) / 2;
+            if (lowerBound > upperBound){
+                return false;
+            }
+            if (a[curIn] == value){
+                break;
+            }else if (a[curIn] < value){
+                lowerBound = curIn + 1;
+            }else{
+                upperBound = curIn - 1;
+            }
+        }
+
+        for (int i = nElems; i < curIn; i++){
+            a[i] = a[i+1];
+        }
+        nElems --;
+        return true;
+        */
     }
 
     public void display(){
@@ -68,37 +140,68 @@ class OrdArray{
         System.out.println();
     }
 
+    public OrdArray merge(OrdArray ordArray){
+        OrdArray dist = new OrdArray(this.nElems + ordArray.nElems);
+        int index = 0;
+
+        for (int i = 0; i < ordArray.size(); i++){
+            dist.insert(ordArray.a[i]);
+        }
+
+        for (int i = 0; i < this.size(); i++){
+            dist.insert(this.a[i]);
+        }
+
+        nElems = ordArray.nElems + this.nElems;
+        return dist;
+
+    }
+
 }
 
 class OrderedApp{
     public static void main(String[] args){
+        Random random = new Random(100);
         int maxSize = 100;
-        OrdArray array = new OrdArray(maxSize);
+        OrdArray array1 = new OrdArray(maxSize);
+        OrdArray array2 = new OrdArray(maxSize);
 
-        array.insert(77);
-        array.insert(99);
-        array.insert(44);
-        array.insert(55);
-        array.insert(22);
-        array.insert(88);
-        array.insert(11);
-        array.insert(00);
-        array.insert(66);
-        array.insert(33);
+        array1.insert(77);
+        array1.insert(99);
+        array1.insert(44);
+        array1.insert(55);
+        array1.insert(22);
+        array1.insert(88);
+        array1.insert(11);
+        array1.insert(00);
+        array1.insert(66);
+        array1.insert(33);
 
+        array2.insert(random.nextInt(20));
+        array2.insert(random.nextInt(50));
+
+        array2.display();
+
+        System.out.println("合并2个数组");
+        OrdArray array3 = array1.merge(array2);
+        array3.display();
+
+        /*
         int searchKey = 33;
-        if (array.find(searchKey) != array.size()){
+        if (array1.find(searchKey) != array1.size()){
             System.out.println("Found " + searchKey);
         }else {
             System.out.println("Can't find " + searchKey);
         }
 
-        array.display();
+        array1.delete(22);
+        array1.display();
 
-        array.delete(00);
-        array.delete(55);
-        array.delete(99);
+        array1.delete(00);
+        array1.delete(55);
+        array1.delete(99);
 
-        array.display();
+        array1.display();
+        */
     }
 }
